257d7b21175c0d1561d7b610643184d0e1975294
mikhail-7975 <mikhail.kalinichenko.1999@gmail.com>
2020-10-13 20:45:07 +0700

    created safe data getting from COM-port
    Проверяется количество точек в трейсе, наличие стоп-символа. В случае ошибки производится повтор запроса на отправку данных.
    
    Добавлена защита от повисаний в виде таймеров

--- auto_COMport_detection.py
+++ auto_COMport_detection.py
@@ -1,8 +1,41 @@
 import serial
 import serial.tools.list_ports
+import matplotlib.pyplot as plt
+import time
 
 porNameList = [i.device for i in serial.tools.list_ports.comports()]
 print(porNameList)
 
 port = serial.Serial(porNameList[0])
-print(port)
+
+while(1):
+    data = []
+    while len(data) < 1002:
+        port.write(b"req")
+        #print("send request...")
+        startTime = time.time()
+        while not port.inWaiting():
+            #print('.')
+            if time.time() - startTime > 1:
+                #print("much time not in waiting")
+                break
+
+        i = 0
+        inp = 0
+        startTime = time.time()
+        while(inp != 254):
+            while port.inWaiting():
+                #print(ord(port.read()), i)
+                inp = ord(port.read())
+                data.append(inp)
+                #print("reading", i)
+                i += 1
+            if time.time() - startTime > 1:
+                #print("much time in trying to read")
+                break
+
+    port.write(b"ack")
+    print(len(data), data)
+    #plt.plot(data)
+    plt.show()
+    print(port)
